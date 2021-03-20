2d5f053f826822d3a6bbe6280ef73833937c8a51
Nicole Rothfield <nicole.rothfield@gmail.com>
2021-03-20 13:20:19 -0500

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,25 +2,42 @@ import os
 import csv
 
 
-
+# set path for file
 BudgetFileName = "budget_data.csv"
 
 CurrentDir = os.getcwd()
 my_file = os.path.join(CurrentDir, 'Resources', BudgetFileName)
 
+date = []
+profitslosses = []
+Totalmonths = []
 
-#set path for file
+# Define the function and have it use the budget data as it's only paramenter
+# def print_analysis(budget_data):
+
+
+
+
+
 with open(my_file, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csv.reader(csvfile) )
-    
-    reader = csv.reader(csvfile)
-    print(csvreader)
-    
-    
-    for row in reader:
+    next(csv.reader(csvfile))
+  
+    for row in csvreader:
 
-        print(row)
+        date.append(row[0])
+
+        profitslosses.append(row[1])
+
+        monthsum = sum(int(row[0]))
+        Totalmonths.append(monthsum)
+
+cleaned_csv = zip(date, profitslosses, Totalmonths)
+
+
+
+
+print(row)
         
    
