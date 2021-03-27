141e1dc3aa0ca17a054b3cc83d7382220d1edaf5
Nicole Rothfield <nicole.rothfield@gmail.com>
2021-03-27 16:45:28 -0500

    PyPoll almost done

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,38 +25,44 @@ with open(my_file, 'r') as csvfile:
     # Create arrays to hold all data that we read in
     Months = []
     ProfitsLosses = []
+    MonthlyChanges = []
+    PriorMonthProfitsLoss = 0
 
     for row in csvreader:
         # Read in the CSV values into the arrays
         Months.append(row[0])
         ProfitsLosses.append(int(row[1]))
+        
+        if len(ProfitsLosses) > 1:
+            MonthlyChanges.append(int(row[1]) - PriorMonthProfitsLoss)
 
+        PriorMonthProfitsLoss = int(row[1])
 
     print(f"Financial Analysis")
     print("-----------------------------")
     print(f"Total Months: {len(Months)}" )
-    print(f"Total: ${locale.currency(sum(ProfitsLosses), grouping=True)}")
-    print(f"Average Change: ${str(round(sum(ProfitsLosses) / len(Months), 2))  }")
+    print(f"Total: {locale.currency(sum(ProfitsLosses), grouping=True)}")
+    print(f"Average Change: ${str(round(sum(MonthlyChanges) / len(MonthlyChanges), 2))  }")
 
     # In order to show the month in which we incurred our greatest profit/loss, we need to first determine
     # the max/min and then use the .index function to determine the CSV line the first occurrance of the max/min
     # happened on. We can then use that to index into the Months array.
-    maxProfit = max(ProfitsLosses)
-    minProfit = min(ProfitsLosses)
+    maxProfit = max(MonthlyChanges)
+    minProfit = min(MonthlyChanges)
 
-    maxProfitMonthIndex = ProfitsLosses.index(maxProfit)
-    minProfitMonthIndex = ProfitsLosses.index(minProfit)
+    maxProfitMonthIndex = MonthlyChanges.index(maxProfit)
+    minProfitMonthIndex = MonthlyChanges.index(minProfit)
 
-    print(f"Greatest Increase in Profits: {Months[maxProfitMonthIndex]} {currencyToString(maxProfit)}")
-    print(f"Greatest Decrease in Profits: {Months[minProfitMonthIndex]} {currencyToString(minProfit)}")
+    print(f"Greatest Increase in Profits: {Months[maxProfitMonthIndex + 1]} {currencyToString(maxProfit)}")
+    print(f"Greatest Decrease in Profits: {Months[minProfitMonthIndex + 1]} {currencyToString(minProfit)}")
 
-file = open("BudgetText.txt","w+")
+file = open("FinancialAnalysis.txt","w+")
 
 file.write(f"Financial Analysis\n")
 file.write("-----------------------------\n")
 file.write(f"Total Months: {len(Months)}\n" )
-file.write(f"Total: ${locale.currency(sum(ProfitsLosses), grouping=True)}\n" )
-file.write(f"Average Change: ${str(round(sum(ProfitsLosses) / len(Months), 2))  }\n")
+file.write(f"Total: {locale.currency(sum(ProfitsLosses), grouping=True)}\n" )
+file.write(f"Average Change: ${str(round(sum(MonthlyChanges) / len(MonthlyChanges), 2))  }\n")
 file.write(f"Greatest Increase in Profits: {Months[maxProfitMonthIndex]} {currencyToString(maxProfit)}\n")
 file.write(f"Greatest Decrease in Profits: {Months[minProfitMonthIndex]} {currencyToString(minProfit)}\n")
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import csv
 #set path for file
 election_file = os.path.join(os.getcwd(), 'Resources', "election_data.csv")
 
+
 with open(election_file, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -12,36 +13,42 @@ with open(election_file, 'r') as csvfile:
     next(csv.reader(csvfile))
 
     # Create arrays to hold all data that we read in
-    Votes = []
-    CandidatesList = []
-    CandidateVoteCount = []
-    Candidates = []
-
+    CandidatesDict = {}
 
     for row in csvreader:
-
         #Read in the CSV values into the arrays
-        
-        Votes.append(int(row[0]))
-        
-        Candidates = (row[2])
-   
-        
-        if Candidates in CandidatesList:
-            CandidateIndex = CandidatesList.index(Candidates)
-            CandidateVoteCount[CandidateIndex] = CandidateVoteCount[CandidateIndex] + 1
+        if row[2] in CandidatesDict:
+            CandidatesDict[row[2]] = CandidatesDict[row[2]] + 1
         else:
-            CandidatesList.append(Candidates)
-            CandidateVoteCount.append(1)
-        
+            CandidatesDict[row[2]] = 1
 
-    print(f"Election Results")
-    print("-----------------------------")
-    print(f"Total Votes: {len(Votes)}" )
-    print("-----------------------------")
-    #print(f"Total: ${locale.currency(sum(ProfitsLosses), grouping=True)}")
-    #print(f"Average Change: ${str(round(sum(ProfitsLosses) / len(Months), 2))  }")
+    SumVotes = sum(CandidatesDict.values())
+
+    OutputLines = []
+    OutputLines.append(f"Election Results ")
+    OutputLines.append("----------------------------- ")
+    OutputLines.append(f"Total Votes: {SumVotes}" )
+    OutputLines.append("-----------------------------")
+
+    for candidateName, voteCount in CandidatesDict.items():
+        percent = round(voteCount / SumVotes * 100, 3)
+        OutputLines.append(f"{candidateName}: {percent}% ({voteCount})")
+
+    #Find the winner through this method 
+    # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+    # Not handling run offs
+    Winner = max(CandidatesDict, key=CandidatesDict.get)
     
-    # In order to show the Candidate that won, we need to first determine
-    # the max/min and then use the .index function to determine the CSV line the first occurrance of the max/min
-    # happened on. We can then use that to index into the Months array.
+    OutputLines.append("-----------------------------")
+    OutputLines.append(f"Winner: {Winner}" )
+    OutputLines.append("-----------------------------")
+
+    for line in OutputLines:
+        print(line)
+
+file = open("PollAnalysis.txt","w+")
+
+for line in OutputLines:
+    file.write(line + "\n")
+
+file.close()
